87538a38c767cc5a0e0dda7d1b893701d9bcae52
John Onyango <j.yayah7@gmail.com>
2020-11-07 15:33:29 +0300

    Reverse Charactersin agiven string

--- src/com/john/ReverseCharacters.java
+++ src/com/john/ReverseCharacters.java
@@ -1,4 +1,32 @@
 package com.john;
 
+import java.util.ArrayList;
+
+
 public class ReverseCharacters {
+    public static void main(String[] args) {
+        String inputString = "rebofaco";
+        String outputString = getReversedString(inputString);
+        System.out.println(outputString);
+        System.out.println(getReversedString(inputString));
+
+    }
+    static String getReversedString(String inputString){
+        ArrayList<String> outputString = new ArrayList<>();
+        int stringLength = inputString.length();
+        int mod = stringLength % 4;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i= 0; i<= (stringLength - mod);  i+=4){
+            outputString.add(inputString.substring(i, i+4));
+        }
+        if ( (inputString.length()) % 4  > 0 ){
+            outputString.add(inputString.substring((stringLength-mod), stringLength));
+        }
+
+        outputString.forEach((string) ->{
+            sb.append(new StringBuilder(string).reverse().toString());
+        });
+        return sb.toString();
+    }
 }
