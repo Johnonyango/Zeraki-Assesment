5821a32fc57fae4e0d47edc9877d9a6a13be2abf
John Onyango <j.yayah7@gmail.com>
2020-11-09 13:32:42 +0300

    add documentation for the project in a readme file

---
+++ Readme.md
@@ -0,0 +1,6 @@
+#Zeraki Assesment Test
+Question 1, 2 and 3 files are in the src folder in the package named com.john.
+
+Question 4 is in the sql sub-package in the com.john package:
+   4.a) Found in the education.sql file
+   4.b) Found in the query.sql file
--- src/com/john/ReverseCharacters.java
+++ src/com/john/ReverseCharacters.java
@@ -5,30 +5,33 @@ import java.util.ArrayList;
 
 public class ReverseCharacters {
     public static void main(String[] args) {
-        String inputString = "Lorem at"; // test string  eroLta m
+        String inputString = "Lorem at"; // test string
         String outputString = getReversedString(inputString);
         System.out.println(outputString);
         System.out.println(getReversedString(inputString));
 
     }
     static String getReversedString(String inputString){
-        ArrayList<String> outputString = new ArrayList<>();
+
+        //arrayList - reference variable to store the output string
+        ArrayList<String> arrayList = new ArrayList<>();
         int stringLength = inputString.length();
+
+        // mod - modulus to after dividing the length of input string by 4
         int mod = stringLength % 4;
         StringBuilder sb = new StringBuilder();
-
         for (int i= 0; i<= (stringLength - mod);  i+=4){
             try {
-                outputString.add(inputString.substring(i, i+4));
+                arrayList.add(inputString.substring(i, i+4));
             }catch (Exception e){
-
+                System.out.println(e.getCause());
             }
         }
         if ( mod != 0 ){
-            outputString.add(inputString.substring((stringLength-mod), stringLength));
+            arrayList.add(inputString.substring((stringLength-mod), stringLength));
         }
 
-        outputString.forEach((string) ->{
+        arrayList.forEach((string) ->{
             sb.append(new StringBuilder(string).reverse().toString());
         });
         return sb.toString();
